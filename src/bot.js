740e62444a3d2999355e5954cc9cf027647b99e0
Janet Mendez <janetmendez.03@gmail.com>
2019-11-11 00:38:20 -0500

    added giphyAPI and made waffles command

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,36 @@
-require('dotenv').config()
+// require('dotenv').config()
 const Discord = require("discord.js")
+const GClient = require('giphy-js-sdk-core')
+const { prefix, token, giphy_token } = require('../config.json')
 const client = new Discord.Client()
+const giphy = GClient(giphy_token )
+// const embed = new Discord.RichEmbed().setColor('#F3DE8A');
 
-client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+client.once('ready', () => {
+    console.log("Connected as " + client.user.tag);
+    console.log("Ready to send waffles!")
+})
+client.on('message', message => {
+	if(message.content.startsWith(`${prefix}waffles`)){
+        let member = message.mentions.members.first();
+        if(!!member){
+            // message.channel.send(`Sending Waffles to @${member.displayName} :cookie:`)
+            //console.log(message.author)
+            // let wafflesEmbed = embed.addField(`Sending Waffles to ${member.displayName} from ${message.author.username}`, ':cookie:')
+            
+            // Look for a waffles GIF
+            giphy.search('gifs', {'q': 'breakfast waffles' }).then((r) => {
+                let random = Math.floor((Math.random() * 10) + 1) % r.data.length
+                let gif = r.data[random]
+                message.channel.send(`Sending Waffles to ${member} from ${message.author}`, {
+                    files: [gif.images.fixed_height.url]
+                })
+            })
+        }
+        else{
+            message.channel.send("Please mention a user to send waffles")
+        }
+    }
 })
 
-client.login(process.env.TOKEN)
+client.login(token)
