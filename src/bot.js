98885a20404aec8d8d8fdc8169c36e703e57f1bc
Janet Mendez <janetmendez.03@gmail.com>
2019-11-11 16:02:18 -0500

    made GIPHY intergration and made new Embeds

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,12 @@ const GClient = require('giphy-js-sdk-core')
 const { prefix, token, giphy_token } = require('../config.json')
 const client = new Discord.Client()
 const giphy = GClient(giphy_token )
-// const embed = new Discord.RichEmbed().setColor('#F3DE8A');
+
+// Fn to create a new Embed with default color
+const createEmbed = () => {
+    let embed = new Discord.RichEmbed().setColor('#F3DE8A')
+    return embed
+}
 
 client.once('ready', () => {
     console.log("Connected as " + client.user.tag);
@@ -12,23 +17,21 @@ client.once('ready', () => {
 })
 client.on('message', message => {
 	if(message.content.startsWith(`${prefix}waffles`)){
+        // Find the first member in a member list
         let member = message.mentions.members.first();
+        // If there is a member mentioned
         if(!!member){
-            // message.channel.send(`Sending Waffles to @${member.displayName} :cookie:`)
-            //console.log(message.author)
-            // let wafflesEmbed = embed.addField(`Sending Waffles to ${member.displayName} from ${message.author.username}`, ':cookie:')
-            
             // Look for a waffles GIF
-            giphy.search('gifs', {'q': 'breakfast waffles' }).then((r) => {
+            giphy.search('gifs', {'q': 'breakfast waffle' }).then((r) => {
                 let random = Math.floor((Math.random() * 10) + 1) % r.data.length
                 let gif = r.data[random]
-                message.channel.send(`Sending Waffles to ${member} from ${message.author}`, {
-                    files: [gif.images.fixed_height.url]
-                })
+                // Send Waffle GIF to user
+                message.channel.send(`${member}`, createEmbed().setTitle(`:sparkling_heart:\tYou have recieved waffles from ${message.author.username}`).setImage(gif.images.fixed_height.url).setFooter('Powered by GIPHY','https://media.giphy.com/avatars/studiosoriginals/j3JBzK5twdv8.jpg'))
             })
         }
+        // Send error messsage to specify a member
         else{
-            message.channel.send("Please mention a user to send waffles")
+            message.channel.send(createEmbed().setTitle(':no_entry:\tPlease mention a user to send waffles'))
         }
     }
 })
